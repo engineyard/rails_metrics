6a700ca210474b3ba8af53e344774156302e8751
Jeff Kunkle <jeff.kunkle@nearinfinity.com>
2011-06-24 22:31:09 -0400

    Remove all but the RAILS_DEFAULT_LOGGER deprecation warnings

--- test/dummy/config/initializers/cookie_verification_secret.rb
+++ test/dummy/config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'db28d7ac0ee4317c045ec2efc0f073c2a0d03abfa90a1a2ea5b0eb5c0a50325792f79be6a0fb3797ec4376e4a9da3edf61dc85569a32e5dcb7e55148d6a49c5a'
+Dummy::Application.config.secret_token = 'db28d7ac0ee4317c045ec2efc0f073c2a0d03abfa90a1a2ea5b0eb5c0a50325792f79be6a0fb3797ec4376e4a9da3edf61dc85569a32e5dcb7e55148d6a49c5a'
